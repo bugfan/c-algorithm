2e3cc81d6dacad251677ce016bd6ce5e14fa4307
Fan <30284615+bugfan@users.noreply.github.com>
2018-06-14 13:55:47 +0800

    字符串实现大数相加

--- main.c
+++ main.c
@@ -24,8 +24,42 @@ int main()
 
     // 桶排序
     BucketSorting();
+    
+    // 大数相加
+//   	// 测试自己的算法
+// 	  al1010("999999999999999999999999999999999999999999999999999999999","999999999999999999999999999999999999999999999999999999999")	// 101011
+
     return 0; 
 }  
+// const(
+// 	HEX=10
+// )
+// func al1010(s1,s2 string){
+// 	log.Println("开始输出1010：")
+// 	max:=s1
+// 	min:=s2
+// 	if len(s1)<len(s2){
+// 		max=s2
+// 		min=s1
+// 	}
+// 	m:=0
+// 	s:=""
+// 	k:=len(min)-1
+// 	for i:=len(max)-1;i>=0;i--{
+// 		t:=int(max[i]-'0') +m
+// 		if k>=0{
+// 			t=t+int(min[k]-'0')
+// 		}
+// 		k--
+// 		s=to.String(t%HEX)+s
+// 		m=t/HEX
+// 		// log.Println("r:",t,s,m)		
+// 	}
+// 	if m==1{
+// 		s=to.String(m)+s
+// 	}
+// 	log.Println("结果:",s)
+// }
 void BucketSorting(){
     printf("开始BucketSorting:\n");
     int arrs[11]={0},res[101]={0},i,j;
